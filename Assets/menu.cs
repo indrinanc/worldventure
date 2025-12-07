6ec1001999f0acb68647e9ca2a1e507405eb56f8
indrinanc <indrinanurchairunnisya@gmail.com>
2025-12-07 13:23:44 +0700

    Initial commit (Unity project)

---
+++ Assets/menu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public GameObject homePanel;
+    public GameObject kreditPanel;
+    public GameObject pilihPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        homePanel.SetActive(true);
+        kreditPanel.SetActive(false);
+        pilihPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void backButton()
+    {
+        homePanel.SetActive(true);
+        kreditPanel.SetActive(false);
+        pilihPanel.SetActive(false);
+    }
+
+    public void kreditButton()
+    {
+        homePanel.SetActive(false);
+        kreditPanel.SetActive(true);
+        pilihPanel.SetActive(false);
+    }
+
+    public void pilihButton()
+    {
+        homePanel.SetActive(false);
+        kreditPanel.SetActive(false);
+        pilihPanel.SetActive(true);
+    }
+
+     public void zooButton(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+     public void parkButton(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+     public void marketButton(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
+
